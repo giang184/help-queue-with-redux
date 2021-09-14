0fcb02fd6bb0673e14d81cb3c60a2b61b8c7ac81
Andrew Giang <giang184@gmail.com>
2021-09-14 09:06:58 -0700

    update map state to props

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -119,7 +119,8 @@ TicketControl.propTypes = {
 
 const mapStateToProps = state => {
   return {
-    masterTicketList: state
+    masterTicketList: state.masterTicketList,
+    formVisibleOnPage: state.formVisibleOnPage
   }
 }
 
